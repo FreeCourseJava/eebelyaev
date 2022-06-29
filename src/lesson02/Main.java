e78820173559ff3a101fbe9035609b16f9d7f712
eebelyaev <eebelyaev@gmail.com>
2022-06-29 17:49:55 +0300

    Устранил bug в changeBalance
    Ранее при вызове changeBalance(-Integer.MAX_VALUE, -1) возвращался 0.

--- src/lesson02/Main.java
+++ src/lesson02/Main.java
@@ -48,7 +48,7 @@ public class Main {
      */
     private static int changeBalance(int balance, int amount) {
         long res = (long) balance + amount;
-        if (Math.abs(res) > Integer.MAX_VALUE) {
+        if (res > Integer.MAX_VALUE || res < Integer.MIN_VALUE) {
             res = 0;
         }
         return (int) res;
